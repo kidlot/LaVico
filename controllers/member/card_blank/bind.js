3cd2a4d4dce228bbddce310391b8d08c57ee22f1
davidxuqq839305878 <1724585767@qq.com>
2014-03-14 15:00:40 +0800

    david.xu bind and unbind

--- controllers/member/card_blank/bind.js
+++ controllers/member/card_blank/bind.js
@@ -9,10 +9,101 @@ module.exports = {
     layout:null
     ,view: 'lavico/templates/member/card_blank/bind.html'
     ,process:function(seed, nut){
+        //nut.disabled = true ;
         var wxid = seed.wxid ? seed.wxid : 'oBf_qJQ8nGyKu5vbnB1_u5okMT6Y';//预先定义微信ID
         nut.model.wxid = wxid ;
     }
     ,viewIn:function(){
+
+            var fourNum;
+            var userTel;
+            var getRandomNum = function(num){
+                //获得一个随机Num位的数
+                var arr =[];
+                for(var i = 1; i <= num; i++){
+                    arr.push(Math.floor(Math.random()*10));
+                }
+                var str = arr.join('');
+                return str;
+            }
+            $('#getCaptcha').click(function(){
+                // 向短信接口发送一个手机号码和短信内容，短信内容包括验证码
+
+                userTel = $('#userTel').val();
+                if(userTel ==''){
+                    alert('请输入手机号码！');
+                    return false;
+                }
+                var reg = /^1[358]\d{9}$/g;
+                if(!reg.test(userTel)){
+                    alert('请输入有效的手机号码！');
+                }
+                fourNum = getRandomNum(4);
+
+                console.log(fourNum);
+            });
+            $('#submit').click(function(){
+                if($('#userCaptcha').val()==''){
+                    alert('请填写验证码');
+                    return false;
+                }
+                if(String($('#userCaptcha').val()) != fourNum){
+                    alert('验证码不正确');
+                    return false;
+                }
+
+                var userCardNumber = $('#userCardNumber').val();
+                var userName = $('#userName').val();
+                var userTel = $('#userTel').val();
+
+                if(userCardNumber==''){
+                    alert('请填写卡号');
+                    return false;
+                }
+                if(userName==''){
+                    alert('请填写手机号吗');
+                    return false;
+                }
+
+                $.ajax({
+                    url:'/lavico/member/card_blank/bind:save',
+                    type:'POST',
+                    data:{
+                        'userCardNumber':userCardNumber,
+                        'userName':userName,
+                        'userTel':userTel
+                    },
+                    success:function(data){
+                        console.log('++++++++++++++++++++++');
+                        console.log(data);
+                        console.log('++++++++++++++++++++++');
+                        var dataJson = eval('(' + data + ')');
+                        if(dataJson.O_ISSUCCEED == 'Y'){
+                            //成功
+                            console.log(dataJson.O_HINT);
+                            alert(dataJson.O_HINT);
+                        }else if(dataJson.O_ISSUCCEED == 'N'){
+                            //失败
+                            console.log(dataJson.O_HINT);
+                            alert(dataJson.O_HINT);
+                        }else{
+                            //其他错误
+                            var otherError = 'O_ISSUCCEED的返回值有误';
+                            console.log(otherError);
+                            console.log(typeof(dataJson));
+                        }
+                    },
+                    error:function(msg){
+                        console.log('----------------------');
+                        alert(msg);
+                        console.log('----------------------');
+
+                    }
+                });
+
+            });
+
+
 
     }
     ,actions:{
@@ -46,7 +137,7 @@ module.exports = {
                         returnDoc.res.write(data);
                         returnDoc.res.end();
                     }));
-
+                    console.log('++++++++++++');
                     //6226045836974321
                 }
         }
