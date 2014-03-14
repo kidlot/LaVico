58d881c5aba648c88df5bc242fe8720d6950c4bd
aaron <11111199@qq.com>
2014-03-14 16:54:30 +0800

    .

--- controllers/bargain/index.js
+++ controllers/bargain/index.js
@@ -1,17 +1,20 @@
 module.exports = {
 
 	layout: "welab/Layout"
-	, view: "lavico/bargain/templates/index.html"
+	, view: "lavico/templates/bargain/index.html"
 
     , process: function(seed,nut)
     {
 
+        var list = {};
+        helper.db.coll("lavico/bargain").find({}).sort({createTime:-1}).page(50,seed.page||1,this.hold(function(err,page){
+            list = page
+        })) ;
 
-
-
+        this.step(function(){
+            nut.model.page = list
+        })
     }
-
-
 }
 
 
