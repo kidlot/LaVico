d5af2cd7c76cbb0a2962deadbede8f2fb67be62f
davidxuqq839305878 <1724585767@qq.com>
2014-03-17 16:32:00 +0800

    member

--- controllers/member/index.js
+++ controllers/member/index.js
@@ -3,8 +3,6 @@
  * 进入会员管理页面，首先先进入本页面，然后根据不同类型的会员进入不同的页面 card_blank/index or card_member/index
  */
 
-
-
 var middleware = require('lavico/lib/middleware.js');//引入中间件
 
 module.exports = {
@@ -15,7 +13,7 @@ module.exports = {
         //根据WXID来判断会员的类型
         //判断会员接口
         //返回值 type = card_blank || card_member
-        var type = 'card_member';
+        var type = 'card_blank';
         if(type == "card_blank"){
             this.res.writeHead(302, {'location':'/lavico/member/card_blank/index?wxid='+wxid})
             //跳转 lavico/member/card_blank/index
