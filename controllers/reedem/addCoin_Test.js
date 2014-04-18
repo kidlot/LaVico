9cfe9a717b4aa3a106cd4b6c25fcb3632e050c80
json <you@example.com>
2014-04-18 12:41:06 +0800

    json4-18

---
+++ controllers/reedem/addCoin_Test.js
@@ -0,0 +1,25 @@
+var middleware = require('../../lib/middleware.js');
+module.exports={
+    layout: "welab/Layout",
+    view:"lavico/templates/reedem/addCoin_Test.html",
+    process:function(seed,nut){
+
+    },
+    actions:{
+        submit:{
+            layout: "welab/Layout",
+            view:"lavico/templates/reedem/addCoin_Test.html",
+            process:function(seed,nut){
+                nut.view.disable();
+                postDate=JSON.parse(seed.postDate)
+                middleware.request('Point/Change',{
+                    memberId:postDate.memberId,
+                    qty:postDate.qty
+                },this.hold(function(err,doc){
+                    if(doc.success)
+                        //nut.message("保存成功",null,'success') ;
+                }))
+            }
+        }
+    }
+}
