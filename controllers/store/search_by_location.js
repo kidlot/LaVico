1758d6dc1f76acd3e053555223c290207e8c6e06
json <you@example.com>
2014-04-28 17:13:04 +0800

    111

--- controllers/store/search_by_location.js
+++
@@ -1,8 +0,0 @@
-var middleware = require('../../lib/middleware.js');
-module.exports={
-    layout: null,
-    view:"lavico/templates/store/searchByLocation.html",
-    process:function(seed,nut){
-
-    }
-}
