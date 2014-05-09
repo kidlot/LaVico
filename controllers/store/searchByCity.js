6b2e26033393ec268763afe79becfbc5d1fa62ca
zhaoyu123 <767486337@qq.com>
2014-05-09 13:44:33 +0800

    zy

--- controllers/store/searchByCity.js
+++ controllers/store/searchByCity.js
@@ -136,14 +136,14 @@ module.exports={
                     var geolocation = new BMap.Geolocation();
 
                     geolocation.getCurrentPosition(function(r){
-                        alert(r);
+
                         r.point.lng=log;
                         r.point.lat=lat;
                         if(this.getStatus() == BMAP_STATUS_SUCCESS){
                             var mk = new BMap.Marker(r.point);
                             map.addOverlay(mk);
                             map.panTo(r.point);
-                            alert('您的位置：'+r.point.lng+','+r.point.lat);
+                            //alert('您的位置：'+r.point.lng+','+r.point.lat);
                         }
                         else {
                             alert('failed'+this.getStatus());
