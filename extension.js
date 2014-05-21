dbf95e63dddce7cd3d4caf86910e57904059fed1
aaron <11111199@qq.com>
2014-05-21 18:47:47 +0800

    test

--- extension.js
+++ extension.js
@@ -8,7 +8,6 @@ var shake = require("./weixinReply/shake.js") ;
 var lookbook = require("./weixinReply/lookbook.js") ;
 var maps = require("./weixinReply/maps.js") ;
 var rewriteWelab = require("./rewriteWelab.js") ;
-var wechatapi = require("welab/lib/wechat-api.js");
 var sportsman = require("./weixinReply/sportsman.js")
 exports.onload = function(application){
 
@@ -35,19 +34,6 @@ exports.onload = function(application){
 
     //型男测试
     sportsman.load();
-
-    wechatapi.registerReply(9, function (params, req, res, next) {
-        if (params.MsgType === "event" && params.Event === "CLICK" && params.EventKey === "registerbutton") {
-
-            params.Content = "registerbutton"
-            var register = require("welab/apps/register/controllers/register.js");
-            register.reply(params,req,res,next)
-        } else {
-            next();
-        }
-    });
-
-    wechatapi.makeQueue();
 
 
     /**
