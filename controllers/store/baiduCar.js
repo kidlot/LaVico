b463356ca7d36aeb3cd6f46e5c65fd6b63376f08
cshj586 <cshj586@163.com>
2014-05-22 17:50:22 +0800

    json

--- controllers/store/baiduCar.js
+++ controllers/store/baiduCar.js
@@ -1,6 +1,6 @@
 
 module.exports= {
-    layout: null,
+    layout: "welab/layout",
     view: "lavico/templates/store/baiduCar.html",
     process: function (seed, nut) {
         nut.model.shopLng=seed.shopLng;//门店经度
