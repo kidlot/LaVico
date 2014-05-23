640c438452bd65e9e581513242d6cad61edfec9f
cshj586 <cshj586@163.com>
2014-05-23 23:09:44 +0800

    json

--- controllers/answerQuestion/first.js
+++ controllers/answerQuestion/first.js
@@ -15,24 +15,16 @@ module.exports= {
             helper.db.coll("welab/customers").findOne({"wechatid":wxid},this.hold(function(err,doc){
                 if(err) throw err;
                 if(doc && doc.HaiLanMemberInfo && doc.HaiLanMemberInfo.memberID && doc.HaiLanMemberInfo.action=='bind'){
-                    //console.log("ffff");
-                    //return doc.HaiLanMemberInfo.memberID;//获取会员ID
+                    nut.model.flag="0";
                 }else{
                     nut.model.flag="1";
-                    //console.log("aa")
-                    //nut.disable();//不显示模版
-
-                    //this.res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
-                    //window.onload=function(){window.popupStyle2.on('您还不是LaVico的会员，请先注册会员',function(event){location.href='/lavico/member/index?wxid="+wechatid+"'})}
-                    //this.res.write("<script>window.onload=function(){window.popupStyle2.on('您还不是LaVico的会员，请先注册会员',function(event){location.href='/lavico/member/index?wxid="+wxid+"'})}");
-                    //this.res.end();
-                    //this.terminate();
                 }
             }))
         })
 
         this.step(function(){
             //var id = '536879ea38c23a370d000c72';
+            console.log("aaa");
             helper.db.coll("lavico/themeQuestion").findOne({"_id":helper.db.id(seed._id)},this.hold(function(err,doc){
                 if(err) throw err
                 if(doc)	nut.model.docs=doc;
