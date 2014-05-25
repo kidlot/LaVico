98ce8a70ad7828f59383625e98241e370852e86e
cshj586 <cshj586@163.com>
2014-05-25 19:42:32 +0800

    store3_num3

--- controllers/answerQuestion/first.js
+++ controllers/answerQuestion/first.js
@@ -38,4 +38,43 @@ module.exports= {
 
     }
 
+}/**
+ * Created by root on 14-5-5.
+ */
+module.exports= {
+    layout: "lavico/layout",
+    view:"lavico/templates/answerQuestion/answer_num1.html",
+    process:function(seed,nut){
+
+        nut.model.wechatId=seed.wechatid;
+        nut.model._id=seed.wechatid;
+        var wxid=seed.wechatid;
+
+        this.step(function(){
+
+            helper.db.coll("welab/customers").findOne({"wechatid":wxid},this.hold(function(err,doc){
+                if(err) throw err;
+                if(doc && doc.HaiLanMemberInfo && doc.HaiLanMemberInfo.memberID && doc.HaiLanMemberInfo.action=='bind'){
+                    nut.model.flag="0";
+                }else{
+                    nut.model.flag="1";
+                }
+            }))
+        })
+
+        this.step(function(){
+            //var id = '536879ea38c23a370d000c72';
+
+            helper.db.coll("lavico/themeQuestion").findOne({"_id":helper.db.id(seed._id)},this.hold(function(err,doc){
+                if(err) throw err
+                if(doc)
+                {
+                    nut.model.goMemberInfo=(docs.explanation).replace("{@wechatid}",seed.wechatid);
+                    nut.model.docs=doc;
+                }
+            }));
+        })
+
+    }
+
 }
