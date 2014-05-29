db18d5edf47b281b06b0b6c1a6efd4ef5b892bea
zhaoyu123 <767486337@qq.com>
2014-05-29 20:32:01 +0800

    zy

--- controllers/answerQuestion/first.js
+++ controllers/answerQuestion/first.js
@@ -1,48 +1,3 @@
-/**
- * Created by root on 14-5-5.
- */
-//module.exports= {
-//    layout: "lavico/layout",
-//    view:"lavico/templates/answerQuestion/answer_num1.html",
-//    process:function(seed,nut){
-//
-//        nut.model.wechatId=seed.wechatid;
-//        nut.model._id=seed.wechatid;
-//        var wxid=seed.wechatid;
-//        var memberid;
-//
-//
-//        this.step(function(){
-//
-//            helper.db.coll("welab/customers").findOne({"wechatid":wxid},this.hold(function(err,doc){
-//                if(err) throw err;
-//                if(doc && doc.HaiLanMemberInfo && doc.HaiLanMemberInfo.memberID && doc.HaiLanMemberInfo.action=='bind'){
-//                    memberid = doc.HaiLanMemberInfo.memberID;
-//                    nut.model.flag="0";
-//                }else{
-//                    nut.model.flag="1";
-//                    memberid = "undefined";
-//                }
-//                nut.model.memberid = memberid;
-//            }))
-//        })
-//
-//        this.step(function(){
-//            helper.db.coll("lavico/themeQuestion").findOne({"wechatid":helper.db.id(wxid)},this.hold(function(err,doc){
-//                if(err) throw err
-//                if(doc)
-//                {
-//                    nut.model.goMemberInfo=(docs.explanation).replace("{@wechatid}",seed.wechatid);
-//                    nut.model.docs=doc;
-//                }
-//            }));
-//        })
-//
-//    }
-//
-//}/**
- //* Created by root on 14-5-5.
- //*/
 module.exports= {
     layout: "lavico/layout",
     view:"lavico/templates/answerQuestion/answer_num1.html",
@@ -52,6 +7,7 @@ module.exports= {
         nut.model._id=seed.wechatid;
         var wxid=seed.wechatid;
         var memberid;
+
         this.step(function(){
 
             helper.db.coll("welab/customers").findOne({"wechatid":wxid},this.hold(function(err,doc){
@@ -68,13 +24,22 @@ module.exports= {
         })
 
         this.step(function(){
-            //var id = '536879ea38c23a370d000c72';
+            if(memberid!="undefined"){
+                helper.db.coll("lavico/custReceive").count({"themeId":helper.db.id(id),"memberId":memberid},this.hold(function(err,doc){
+                    if(err) throw err;
+                    if(doc>0){
+                        this.res.writeHead(302, {'Location': "/lavico/answerQuestion/answer?_id="+id+"&optionId=1&wechatid="+wxid});
+                        this.res.end();
+                    }
+                }))
+            }
+        })
+
+        this.step(function(){
             console.log(seed._id);
             helper.db.coll("lavico/themeQuestion").findOne({"_id":helper.db.id(seed._id)},this.hold(function(err,doc){
                 if(err) throw err
-                if(doc)
-                {
-                    //nut.model.goMemberInfo=doc.explanation;
+                if(doc){
                     nut.model.docs=doc;
                 }
             }));
