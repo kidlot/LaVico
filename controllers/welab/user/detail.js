2fa4796332b032283be0c6f26bcf3272a9f78835
aaron <11111199@qq.com>
2014-05-29 15:49:26 +0800

    kk

--- controllers/welab/user/detail.js
+++ controllers/welab/user/detail.js
@@ -1,7 +1,7 @@
 /**
  * Created by David Xu on 5/29/14.
  */
-var summary = require("./summary.js") ;
+var summary = require("welab/controllers/user/summary.js") ;
 
 module.exports = {
 
@@ -10,30 +10,9 @@ module.exports = {
 
     ,process: function (seed, nut) {
 
-        helper.db.coll("welab/customers").findOne({_id : helper.db.id(seed._id)},this.hold(function(err,doc){
 
-            var _data = {};
-            _data = doc;
-            summary.otherData(seed._id , this.hold(function(doc){
-                _data.otherData = doc;
+        console.log("fffffffff")
 
-                _data.regData = {}
-                for(var _tmp in _data){
-                    if( _tmp != "_id" && _tmp != "city" && _tmp != "country" && _tmp != "face" && _tmp != "fakeid" && _tmp != "followCount" && _tmp != "followTime" && _tmp != "gender" && _tmp != "isFollow" && _tmp != "lastMessageTime" && _tmp != "messageCount" && _tmp != "province" && _tmp != "ranking" && _tmp != "realname" && _tmp != "wechatUsername" && _tmp != "wechatid" && _tmp != "otherData" && _tmp!="regData" && _tmp!="birthday" && _tmp!="mobile" && _tmp!="tags" && _tmp!="email" && _tmp!="registerTime" && _tmp!="telephone" && _tmp!="unfollowCount" && _tmp!="unfollowTime"  && _tmp!="shareFriendCount" && _tmp!="shareTimeLineCount" && _tmp!="viewCount" && _tmp!="viewTimeLineCount"){
-                        _data.regData[_tmp] = _data[_tmp]
-                    }
-                }
-
-                if(_data.face && !/^http:\/\//g.test(_data.face) && !/^\/welab\//g.test(_data.face)){
-                    _data.face = "/welab/" + _data.face
-                }
-
-                console.log(_data)
-
-
-                nut.model.data = _data||{};
-            }));
-        }))
     }
 
     , viewIn: function(){
