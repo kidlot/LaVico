ef6ead78420d097ab2367cc9df83b69c0c17f649
davidxuqq839305878 <1724585767@qq.com>
2014-05-29 18:45:58 +0800

    share

--- controllers/layout.js
+++ controllers/layout.js
@@ -16,18 +16,19 @@ module.exports = {
                 WeixinJSBridge.call('hideOptionMenu');
             });
         }
-        function hideShareButton(){
-            if (typeof WeixinJSBridge == "undefined"){
-                if( document.addEventListener ){
-                    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-                }else if (document.attachEvent){
-                    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-                    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-                }
-            }else{
-                onBridgeReady();
+        function hideShareButton(){ }
+
+        if (typeof WeixinJSBridge == "undefined"){
+            if( document.addEventListener ){
+                document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+            }else if (document.attachEvent){
+                document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
             }
+        }else{
+            onBridgeReady();
         }
+
         /*掩藏分享按钮End*/
     }
 }
