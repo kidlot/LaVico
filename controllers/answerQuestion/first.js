f999fd1f7c211855fbf4d881654c5afd13035bf8
cshj586 <cshj586@163.com>
2014-06-05 16:01:32 +0800

    zy

--- controllers/answerQuestion/first.js
+++ controllers/answerQuestion/first.js
@@ -151,7 +151,7 @@ module.exports= {
             }else{
                 chooseNext = "-1";
             }
-            console.log(chooseNext)
+            console.log("chooseNext:"+chooseNext)
             nut.model.choose = chooseNext;
         })
 
