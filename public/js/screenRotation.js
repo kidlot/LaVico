37ca214d1aeaceda2febf1ee85ccea23d49cfd60
zlww613 <zlww613@sina.com>
2014-06-06 20:04:29 +0800

    a

--- public/js/screenRotation.js
+++ public/js/screenRotation.js
@@ -12,20 +12,10 @@ var ua = navigator.userAgent;
 		viewport.attr('content', 'width=1000, initial-scale=0.5, minimum-scale=0.5, maximum-scale=0.5, user-scalable=yes');
 	}
 
-	function modalOrientation() {
-	    var orientation = document.getElementById("orientation");
-	    var orientationW = document.getElementById("orientation").offsetWidth;
-	    var orientationH = document.getElementById("orientation").offsetHeight;
-	    orientation.style.marginLeft = -orientationW / 2;
-	    orientation.style.marginTop = -orientationH / 2;
-	}
-
 	$(window).on('orientationchange', function() {
 		if (deviceType === 'isIphone') {
 			if (Math.abs(window.orientation) === 90) {
 			    $('.backdrop-orientation, .modal-orientation').show();
-			    
-			    modalOrientation();
 			}
 			else {
 				$('.backdrop-orientation, .modal-orientation').hide();
@@ -35,8 +25,6 @@ var ua = navigator.userAgent;
 			if (Math.abs(window.orientation) === 90) {
 				viewport.attr('content', 'width=1000, initial-scale=0.5, minimum-scale=0.5, maximum-scale=0.5, user-scalable=yes');
 				$('.backdrop-orientation, .modal-orientation').show();
-
-				modalOrientation();
 			}
 			else {
 				viewport.attr('content', 'width=640, initial-scale=0.5, minimum-scale=0.5, maximum-scale=0.5, user-scalable=yes');
