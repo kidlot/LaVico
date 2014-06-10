734d87c0af98d081a7f6e14c31fd70ed927400dd
wpsite@qq.com <wpsite@qq.com>
2014-06-10 18:50:19 +0800

    code_id

--- controllers/member/card_blank/register.js
+++ controllers/member/card_blank/register.js
@@ -249,7 +249,7 @@ module.exports = {
                         flag = 0;
                     }else if(data.result == 'ok'){
                         set_interval();
-                        $('#id_code').val(data.id_code);
+                        //$('#id_code').val(data.id_code);
                         console.log('验证码发送成功，请在2分钟内输入');
                     }else{
 
