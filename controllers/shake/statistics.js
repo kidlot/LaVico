a2002d4e8b4b01c7818a07b18eee203c569ee9a4
davidxuqq839305878 <1724585767@qq.com>
2014-06-16 09:52:00 +0800

    shake

--- controllers/shake/statistics.js
+++ controllers/shake/statistics.js
@@ -1,8 +1,8 @@
 /**
  * 摇一摇统计页面
  * URL：/lavico/shake/statistics?_id=536c72da6c637d35587c87b9
+ * by David.xu
  * */
-
 
     module.exports = {
 
@@ -23,10 +23,10 @@
         this.step(function(){
             var count = 0;
             helper.db.coll("lavico/shake").findOne({_id:helper.db.id(seed._id)},this.hold(function(err,_doc){
-                docs = _doc
-
+                docs = _doc;//获取摇一摇活动信息
+                //docs._id.toString()活动唯一标识
                 helper.db.coll("welab/customers").find({"shake.aid":docs._id.toString()}).toArray(then.hold(function(err,_doc2){
-                    docs.sumFavorites = _doc2.length;
+                    docs.sumFavorites = _doc2.length;//用户表里的摇一摇记录
                     for(var i=0;i<_doc2.length;i++){
                       for(var j=0;j<_doc2[i].shake.length;j++){
                        if(_doc2[i].shake[j].aid == docs._id.toString()){
