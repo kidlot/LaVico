ef02522f307cf2cc40ed2684d2fb676133cd8bef
aaron <11111199@qq.com>
2014-06-27 20:56:15 +0800

    bug

--- controllers/store/currentCustomerLocation.js
+++ controllers/store/currentCustomerLocation.js
@@ -5,12 +5,11 @@ module.exports={
     process:function(seed,nut){
         //接口读取门店列表(设置1000代表每页条数，即一次性全部返回)
 
-        nut.model.wxid = seed.wxid ? seed.wxid : 'undefined';
-        console.log("wxid:"+seed.wxid);
+        nut.model.wxid = seed.wxid || 'undefined';
 
         //从数据库获取坐标
         this.step(function(){
-             if(seed.wxid!="" && seed.wxid!="underfined"){
+             if(seed.wxid!="" && seed.wxid!="undefined"){
                  helper.db.coll("welab/customers").findOne({"wechatid":seed.wxid},this.hold(function(err,doc) {
                          if (err) throw err;
                          if (doc && doc.location) {
@@ -35,55 +34,62 @@ module.exports={
              }
         })
 
-    },
-    viewIn:function(){
-        $('#loading').hide();//隐藏加载框
+        this.step(function(){
+            //console.log(nut.model.locationLat)
+            //console.log(nut.model.locationLng)
+        })
+
     },
    actions:{
         show: {
-            layout:"lavico/layout",
+            layout:null,
             view:"lavico/templates/store/store_num2.html",
             process: function (seed, nut){
-                nut.model.wxid = seed.wxid ? seed.wxid : 'undefined';
+                nut.model.wxid = seed.wxid || 'undefined';
                 this.step(function () {
-                var jsonData = {}
-                jsonData.perPage = 1000;
-                jsonData.pageNum = 1;
-                //接口返回的doc都是字符串
-                middleware.request('Shops', jsonData,this.hold(function (err, doc) {
-                    if (err) throw err;
-                    return JSON.parse(doc);//注意字符串和对象格式
-                    }))
+                    var jsonData = {}
+                    jsonData.perPage = 1000;
+                    jsonData.pageNum = 1;
+                    //接口返回的doc都是字符串
+                    middleware.request('Shops', jsonData,this.hold(function (err, doc) {
+                        if (err) throw err;
+                            return JSON.parse(doc);//注意字符串和对象格式
+                        }))
                 })
                 var list1 = [];
+
                 this.step(function (doc) {
 
-                var cityName = (decodeURIComponent(seed.cityName)).substring(0,seed.cityName.length-1);
-                    console.log("cityName",cityName)
+                    var cityName = decodeURIComponent(seed.cityName)
 
-                for (var i = 0; i < doc.list.length; i++) {
-                    if (doc.list[i].CITY == cityName) {
-                        var newCODE = (doc.list[i].CODE).replace(/\s/g, '');
-                        doc.list[i].CODE = newCODE;
-                        if(doc.list[i].ACT){
-                            if(doc.list[i].ACT.length>10){
-                                var act = doc.list[i].ACT.replace(/<[\/]*br[^>]*>/img, "")
-                                doc.list[i].ACT = act.substr(0,30)+"......";
+                    for (var i = 0; i < doc.list.length; i++) {
+
+                        var _reg = eval("/"+cityName+"/")
+                        if (doc.list[i].ADDR && doc.list[i].ADDR.match(_reg)) {
+
+                            var newCODE = (doc.list[i].CODE).replace(/\s/g, '');
+                            doc.list[i].CODE = newCODE;
+                            if(doc.list[i].ACT){
+                                if(doc.list[i].ACT.length>10){
+                                    var act = doc.list[i].ACT.replace(/<[\/]*br[^>]*>/img, "")
+                                    doc.list[i].ACT = act.substr(0,30)+"......";
+                                }
                             }
-                        }
-                        list1.push(doc.list[i]);
+                            list1.push(doc.list[i]);
 
+                        }
                     }
-                }
-                if(list1.length>0){
-                    nut.model.cityName=seed.cityName;
-                    nut.model.city_docs = list1;
-                    nut.model.currentCityName = seed.city;
-                }else{
-                    nut.view.disable();
-                    nut.write("<script>window.onload=function(){window.popupStyle2.on('很抱歉，本城市没有LaVico品牌专柜，请选择其他城市查询',function(event){location.href='javascript:history.back()'})}</script>");
-                }
-            })
+
+                    console.log(list1)
+                    if(list1.length>0){
+                        nut.model.cityName=seed.cityName;
+                        nut.model.city_docs = list1;
+                        nut.model.currentCityName = seed.city;
+                    }else{
+                        nut.view.disable();
+                        nut.write("<script>window.popupStyle2.on('很抱歉，本城市没有LaVico品牌专柜，请选择其他城市查询',function(event){location.href='javascript:history.back()'})</script>");
+                    }
+                })
             }
         }
     }
