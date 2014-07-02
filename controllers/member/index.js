cefd49ef194698561a141fa08ecd4571b5f38bc2
zhaoyu123 <767486337@qq.com>
2014-07-02 18:36:27 +0800

    zy

--- controllers/member/index.js
+++ controllers/member/index.js
@@ -221,29 +221,29 @@ module.exports = {
             }));
         })
 
-        this.step(function(){
-            if(resultlist.length>0){
-                if(typeof(announcement)!="undefined"){
-                    for(var i=0;i<resultlist.length;i++){
-                        for(var j=0;j<announcement.length;j++){
-                            if(announcement[j].toString()==resultlist[i]._id.toString()){
-                                resultlist[j].read="true";
-                            }
-                        }
-                    }
-                    for(var j=0;j<resultlist.length;j++){
-                        if(!resultlist[j].read){
-                            readcount++;
-                        }
-                    }
-                }else{
-                    readcount=1;
-                }
-            }else{
-                readcount=0;
-            }
+//        this.step(function(){
+//            if(resultlist.length>0){
+//                if(typeof(announcement)!="undefined"){
+//                    for(var i=0;i<resultlist.length;i++){
+//                        for(var j=0;j<announcement.length;j++){
+//                            if(announcement[j].toString()==resultlist[i]._id.toString()){
+//                                resultlist[j].read="true";
+//                            }
+//                        }
+//                    }
+//                    for(var j=0;j<resultlist.length;j++){
+//                        if(!resultlist[j].read){
+//                            readcount++;
+//                        }
+//                    }
+//                }else{
+//                    readcount=1;
+//                }
+//            }else{
+//                readcount=0;
+//            }
             nut.model.count = readcount;
-        })
+//        })
 
     },
     viewIn:function(){
