62ca6c2307143484e54ffd9a8d6da80a9af56397
davidxuqq839305878 <1724585767@qq.com>
2014-07-07 18:33:10 +0800

    coupon-info

--- extension.js
+++ extension.js
@@ -9,6 +9,7 @@ var lookbook = require("./weixinReply/lookbook.js") ;
 var maps = require("./weixinReply/maps.js") ;
 var rewriteWelab = require("./rewriteWelab.js") ;
 var sportsman = require("./weixinReply/sportsman.js");
+var statistics = require("./weixinReply/statistics.js") ;
 var wechatapi = require("welab/lib/wechat-api.js") ;
 exports.onload = function(application){
 
@@ -35,6 +36,9 @@ exports.onload = function(application){
 
     //型男测试
     sportsman.load();
+
+    //应用统计
+    //statistics.load();
 
 
     // 覆盖WELAB的LAYOUT
