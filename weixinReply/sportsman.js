8c25aaa1812d99f3abce8d87842d3fccb2c6b4c8
zhaoyu123 <767486337@qq.com>
2014-07-14 14:52:07 +0800

    zy

--- weixinReply/sportsman.js
+++ weixinReply/sportsman.js
@@ -17,13 +17,13 @@
             btns: [
                 {
                     title: '列表',
-                    controller: '/lavico/answerQuestion/themeList?themetype=1'
+                    controller: '/lavico/answerQuestion/themeList'
                 }, {
                     title: '添加',
                     controller: '/lavico/answerQuestion/question/addQuestion?themetype=1'
                 },{
                     title: '统计',
-                    controller: '/lavico/answerQuestion/statistics/statistics_list?themetype=1'
+                    controller: '/lavico/answerQuestion/statistics/statistics_list_1'
                 }
             ],
             start: function() {
