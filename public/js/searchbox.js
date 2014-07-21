ef44d92b62aa26b3d3723a8d9d6bb22a6465d4c8
davidxuqq839305878 <1724585767@qq.com>
2014-07-21 15:09:53 +0800

    user-list

--- public/js/searchbox.js
+++ public/js/searchbox.js
@@ -83,16 +83,38 @@ $.fn.searchConditions = function(){
         var data = [ $(this).find("[name=searchFieldName]").val(), value ] ;
 
         /*2014-07-21添加时间间隔晒选功能*/
-        if(data[0] == "followTime"){
+        if(data[0] == "followTime" || data[0] == "registerTime"){
             data[1] = data[1];//开始时间
             data[2] = $(this).find("[name=searchConditionValueEnd]").val();//结束时间
-
+            var a = /^\d{4}-\d{1,2}-\d{1,2}$/;
+            var _str;
+            if(data[0] == "followTime"){
+                _str = '关注时间';
+            }
+            if(data[0] == "registerTime"){
+                _str = '注册时间';
+            }
+            if (a.test(data[1])) {
+                var _sTime = new Date(data[1] + " 00:00:00").getTime();//开始时间
+                if (data[2]&&a.test(data[2])){
+                    var _tTime = new Date(data[2] + " 23:59:59").getTime();//结束时间
+                }else{
+                    var _tTime = new Date(data[1] + " 23:59:59").getTime();//结束时间
+                }
+                if(_sTime > _tTime){
+                    $.globalMessenger().post({
+                        message: _str+"的开始时间必须比结束时间早！",
+                        type: 'error',
+                        showCloseButton: true})
+                    return;
+                }
+            }
         }
-        if(data[0] == "registerTime"){
-            data[1] =  data[1];//开始时间
-            data[2] = $(this).find("[name=searchConditionValueEnd]").val();//结束时间
-
-        }
+//        if(data[0] == "registerTime"){
+//            data[1] =  data[1];//开始时间
+//            data[2] = $(this).find("[name=searchConditionValueEnd]").val();//结束时间
+//
+//        }
 
         if(data[0] == "HaiLanMemberInfo.type"){
             data[1] = parseInt(data[1]);
