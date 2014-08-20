0ec5212afec03901adc64506bd6e8390799aa45c
davidxuqq839305878 <1724585767@qq.com>
2014-08-20 14:22:20 +0800

    shake-add-file

---
+++ controllers/activity/shake_end03.js
@@ -0,0 +1,88 @@
+/**
+ * Created by David Xu on 5/9/14.
+ */
+var middleware = require('../../lib/middleware.js');
+module.exports = {
+    layout: "lavico/layout",
+    view: "lavico/templates/activity/shake_end.html",
+    process:function(seed,nut){
+
+        var uid = seed.uid || 'undefined';//用户微信ID
+        var aid = seed.aid || 'undefined';//表lavico/shake，唯一确定shake的活动,也就是_id
+        var PROMOTION_CODE = seed.activity || 'undefined';
+        var coupon_no = seed.coupon_no || 'undefined';//优惠券
+        var lottery_info = {};
+        var time = (new Date()).getTime();
+        nut.model.time = time;
+
+        this.step(function(){
+            if(uid == 'undefined'){
+                nut.disable();//不显示模版
+                this.res.writeHead(200, { 'Content-Type': 'application/json' });
+                this.res.write('{"error":"wxid_is_empty"}');
+                this.res.end();
+                this.terminate();
+            }
+
+            if(aid == 'undefined'){
+                nut.disable();//不显示模版
+                this.res.writeHead(200, { 'Content-Type': 'application/json' });
+                this.res.write('{"error":"aid_is_empty"}');
+                this.res.end();
+                this.terminate();
+            }
+
+            if(coupon_no == 'undefined'){
+                nut.disable();//不显示模版
+                this.res.writeHead(200, { 'Content-Type': 'application/json' });
+                this.res.write('{"error":"coupon_no_is_empty"}');
+                this.res.end();
+                this.terminate();
+            }
+        });
+        this.step(function(){
+            helper.db.coll('welab/customers').findOne({wechatid:uid},this.hold(function(err, doc){
+                if(!doc){
+                    nut.disable();//不显示模版
+                    this.res.writeHead(200, { 'Content-Type': 'application/json' });
+                    this.res.write('{"error":"wxid_no_bind_to_welab"}');
+                    this.res.end();
+                    this.terminate();
+                }
+            }));
+        });
+
+        this.step(function(){
+
+            helper.db.coll('lavico/shake').findOne({_id:helper.db.id(aid)},this.hold(function(err, doc){
+                if(!doc){
+                    nut.disable();//不显示模版
+                    this.res.writeHead(200, { 'Content-Type': 'application/json' });
+                    this.res.write('{"error":"aid_no_bind_to_welab"}');
+                    this.res.end();
+                    this.terminate();
+                }else{
+
+                    for(var _i=0;_i<doc.lottery.length;_i++){
+                        if(doc.lottery[_i].PROMOTION_CODE == PROMOTION_CODE){
+                            lottery_info = doc.lottery[_i];
+                        }
+                    }
+                    nut.model.lottery_info = lottery_info;
+                }
+            }));
+        });
+
+        this.step(function(){
+            nut.model.uid = uid;
+        });
+
+
+
+    },
+    viewIn:function(){
+        /*掩藏分享按钮*/
+        window.hideShareButtion.on();
+
+    }
+}
