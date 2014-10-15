1fdbe8a5ab7f888dba83d62d2ec32296103c152b
zhaoyu123 <767486337@qq.com>
2014-10-15 17:51:21 +0800

    zy

--- controllers/test.js
+++ controllers/test.js
@@ -16,28 +16,30 @@ module.exports = {
         this.step(function(){
             if(list.length>0){
                 for(var i=0;i<list.length;i++){
-                    var wxid = list[i].wechatid;
-                    middleware.getUserNickname(wxid,function(err,doc){
-                        if(err){
-                            console.log("err",err);
-                        }
-                        console.log("doc",doc)
-                        if(doc){
-                            console.log("wechatid",wxid)
-                            helper.db.coll("welab/customers_copy").update({"wechatid": wxid}, {$set: {
-                                userName: doc.nickname,
-                                nickname: doc.nickname,
-                                gender: doc.sex,
-                                language: doc.language,
-                                city: doc.city,
-                                province: doc.province,
-                                country: doc.country,
-                                face: doc.headimgurl
-                            }}, {upsert: true}, function (err, doc) {
-                                err && console.log(err)
-                            })
-                        }
-                    })
+                    (function(wxid,i){
+                        middleware.getUserNickname(wxid,function(err,doc){
+                            if(err){
+                                console.log("err",err);
+                            }
+                            console.log("doc",doc)
+                            if(doc){
+                                console.log("wechatid",wxid)
+                                helper.db.coll("welab/customers_copy").update({"wechatid": wxid}, {$set: {
+                                    userName: doc.nickname,
+                                    nickname: doc.nickname,
+                                    gender: doc.sex,
+                                    language: doc.language,
+                                    city: doc.city,
+                                    province: doc.province,
+                                    country: doc.country,
+                                    face: doc.headimgurl
+                                }}, {upsert: true}, function (err, doc) {
+                                    err && console.log(err)
+                                })
+                            }
+                        })
+                    })(list[i].wechatid,i)
+
 
                 }
             }
