f79e0ea290476576908b9eac419918155c604649
zhaoyu123 <767486337@qq.com>
2014-10-15 18:25:24 +0800

    zy

--- controllers/test.js
+++ controllers/test.js
@@ -4,6 +4,7 @@ module.exports = {
     view: null,
     process: function(seed,nut){
         var list = [];
+        var AccessToken="";
         this.step(function(){
             helper.db.coll("welab/customers_copy").find({"nickname":null}).toArray(this.hold(function(err,doc){
                 if(err) console.log("customers_err",err)
@@ -14,15 +15,25 @@ module.exports = {
         })
 
         this.step(function(){
+            middleware.getAccessToken(function(err,doc){
+                if(err) console.log("AccessToken_err",err);
+                console.log("AccessToken",doc)
+                if(doc && doc.access_token){
+                    AccessToken = doc.access_token;
+                }
+            })
+        })
+
+        this.step(function(){
             if(list.length>0){
                 for(var i=0;i<list.length;i++){
                     (function(wxid,i){
-                        middleware.getUserNickname(wxid,function(err,doc){
+                        middleware.getUserNickname(wxid,AccessToken,function(err,doc){
                             if(err){
                                 console.log("err",err);
                             }
                             console.log("doc",doc)
-                            if(doc){
+                            if(doc && doc.nickname){
                                 console.log("wechatid",wxid)
                                 helper.db.coll("welab/customers_copy").update({"wechatid": wxid}, {$set: {
                                     userName: doc.nickname,
@@ -39,8 +50,6 @@ module.exports = {
                             }
                         })
                     })(list[i].wechatid,i)
-
-
                 }
             }
 
