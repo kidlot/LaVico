7f16dbcd6395084990f9542fb263711289f16246
zhaoyu123 <767486337@qq.com>
2015-02-28 17:03:06 +0800

    zy

--- controllers/layout.js
+++ controllers/layout.js
@@ -108,10 +108,6 @@ module.exports = {
 
         $.getJSON('http://wx.lavicouomo.com/welab/api/jssdk', function(data) {
             wx.config(data);
-            var title = "lavico测试！！！_分享标题";
-            var decs = "lavico测试！！！_分享描述";
-            var link = "http://www.baidu.com"
-            var imgurl = "http://g.hiphotos.baidu.com/album/crop%3D107%2C2%2C731%2C303%3Bh%3D240/sign=42681c5d1bd8bc3ed2475c8abfba9629/c9fcc3cec3fdfc03bcac4adfd63f8794a5c22684.jpg";
             wx.ready(function(){
                 //隐藏右上角菜单接口
                 //wx.hideOptionMenu();
@@ -127,11 +123,26 @@ module.exports = {
                 });
                 //分享到朋友圈
                 wx.onMenuShareTimeline({
-                    title: title, // 分享标题
-                    link: link, // 分享链接
-                    imgUrl: imgurl, // 分享图标
+                    title: shareTitle, // 分享标题
+                    link: replaceWxid(location.href) + "fromWelab=timeline", // 分享链接
+                    imgUrl: shareIcon, // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
+                        $.ajax({
+                            url:'/lavico/log',
+                            type:'POST',
+                            data:{
+                                'wxid':getWxid(),
+                                'url':location.href,
+                                'replyid':getReplyID(),
+                                'action' : "share.timeline"
+                            },
+                            success:function(data){
+
+                            },
+                            error:function(msg){
+                            }
+                        });
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
@@ -139,14 +150,29 @@ module.exports = {
                 });
                 //分享给朋友
                 wx.onMenuShareAppMessage({
-                    title: title, // 分享标题
-                    desc: decs, // 分享描述
-                    link: link, // 分享链接
-                    imgUrl: imgurl, // 分享图标
+                    title: shareTitle, // 分享标题
+                    desc: shareContent, // 分享描述
+                    link: replaceWxid(location.href) + "fromWelab=friend", // 分享链接
+                    imgUrl: shareIcon, // 分享图标
                     type: '', // 分享类型,music、video或link，不填默认为link
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
                         // 用户确认分享后执行的回调函数
+                        $.ajax({
+                            url:'/lavico/log',
+                            type:'POST',
+                            data:{
+                                'wxid':getWxid(),
+                                'url':location.href,
+                                'replyid':getReplyID(),
+                                'action' : "share.friend"
+                            },
+                            success:function(data){
+
+                            },
+                            error:function(msg){
+                            }
+                        });
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
